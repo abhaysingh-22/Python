5fd4bb7ed11627715a6a7b5f6c5ee10346ed4838
myfirstrepo <abhaysingh6575@gmail.com>
2025-07-27 09:43:17 +0530

    Enhance chai.py to include reference count tracking before and after calling chai function

--- Chai Aur Code/chai.py
+++ Chai Aur Code/chai.py
@@ -1,5 +1,10 @@
 # now i want to print that method written in hello_chai.py
 
 from hello_chai import chai
+import sys
+
+name = "singh"
+print("Ref count before: ", sys.getrefcount(name))
 
 chai("singh")
+print("Ref count after: ", sys.getrefcount(name))
