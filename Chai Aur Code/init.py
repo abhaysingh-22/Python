e121ca37dff94c27333950317ad74cf1fc87e9e6
myfirstrepo <abhaysingh6575@gmail.com>
2025-07-28 18:39:51 +0530

    Refactor age classification logic in init.py by removing commented-out code

--- Chai Aur Code/init.py
+++ Chai Aur Code/init.py
@@ -2,12 +2,21 @@
 # print("Your score is:", userscore)
 
 
+#question1
+# age = int(input("Enter your age: "))
+# if age < 13:
+#     print("You are a child.")
+# elif 13 <= age < 20:
+#     print("You are a teenager.")
+# elif 20 <= age < 60:
+#     print("You are an adult.")
+# else:
+#     print("You are a senior citizen.")
+    
+    
+#question2
 age = int(input("Enter your age: "))
-if age < 13:
-    print("You are a child.")
-elif 13 <= age < 20:
-    print("You are a teenager.")
-elif 20 <= age < 60:
-    print("You are an adult.")
-else:
-    print("You are a senior citizen.")
+day = "Wednesday"
+
+price = 12 if age >= 18 else 8
+print("the price of the movie ticket is: ", price)
