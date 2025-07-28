6895584bcfe11deea08ef9c556f4b67dd5d4e126
myfirstrepo <abhaysingh6575@gmail.com>
2025-07-28 18:33:51 +0530

    Remove user score input code and retain age classification logic in init.py

--- Chai Aur Code/init.py
+++ Chai Aur Code/init.py
@@ -1,2 +1,13 @@
-userscore = int(input("Enter your score: "))
-print("Your score is:", userscore)
+# userscore = int(input("Enter your score: "))
+# print("Your score is:", userscore)
+
+
+age = int(input("Enter your age: "))
+if age < 13:
+    print("You are a child.")
+elif 13 <= age < 20:
+    print("You are a teenager.")
+elif 20 <= age < 60:
+    print("You are an adult.")
+else:
+    print("You are a senior citizen.")
