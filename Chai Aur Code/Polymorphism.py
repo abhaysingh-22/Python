01969918edcb22f5929ec7cb57e1f6adff7769a6
myfirstrepo <abhaysingh6575@gmail.com>
2025-07-30 22:24:27 +0530

    Add Car and ElectricCar classes demonstrating polymorphism with fuel type methods

---
+++ Chai Aur Code/Polymorphism.py
@@ -0,0 +1,36 @@
+class Car:
+    def __init__(self, brand, model):
+        self.__brand = brand
+        self.model = model
+        
+    def get_brand(self):
+        return self.__brand + " is the brand of the car."
+        
+    def full_name(self):
+        return f"{self.__brand} {self.model}"
+
+    def fuel_type(self):
+        return "This car uses petrol or diesel."
+
+class ElectricCar(Car):
+    def __init__(self, brand, model, battery_size):
+        super().__init__(brand, model)
+        self.battery_size = battery_size
+        
+    def fuel_type(self):
+        return "This car uses electricity."
+
+    def battery_info(self):
+        return f"{self.__brand} {self.model} has a {self.battery_size} kWh battery."
+
+
+
+my_electric_car = ElectricCar("Tesla", "Model S", 100)
+print(my_electric_car.fuel_type())
+
+my_car = Car("Toyota", "Corolla")
+print(my_car.fuel_type())
+
+
+# here, you can see that how polymorphism works in python.
+# The method fuel_type() is defined in both Car and ElectricCar classes.
